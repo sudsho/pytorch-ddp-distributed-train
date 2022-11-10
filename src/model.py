2eb6e97f3a12b940008e4a9ed52c6427a164fe16
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-11-10 19:04:51 +0530

    clean up that ugly torch import in model.py

--- src/model.py
+++ src/model.py
@@ -1,14 +1,17 @@
 """Model factory. Currently just torchvision resnets."""
+import torch.nn as nn
 from torchvision import models
 
 
 def build_model(arch="resnet50", num_classes=10, pretrained=False):
     if arch == "resnet50":
-        m = models.resnet50(weights=None if not pretrained else models.ResNet50_Weights.DEFAULT)
+        weights = models.ResNet50_Weights.DEFAULT if pretrained else None
+        m = models.resnet50(weights=weights)
     elif arch == "resnet18":
-        m = models.resnet18(weights=None if not pretrained else models.ResNet18_Weights.DEFAULT)
+        weights = models.ResNet18_Weights.DEFAULT if pretrained else None
+        m = models.resnet18(weights=weights)
     else:
         raise ValueError(f"unknown arch: {arch}")
     in_feats = m.fc.in_features
-    m.fc = __import__("torch").nn.Linear(in_feats, num_classes)
+    m.fc = nn.Linear(in_feats, num_classes)
     return m
