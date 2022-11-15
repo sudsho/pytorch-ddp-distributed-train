ed5b8af50ef795a1a60f2f3ac2349b4bd6777bff
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-11-16 01:11:53 +0530

    mlflow hooks (rank-0 only)

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,11 @@ import os
 import time
 import yaml
 
+try:
+    import mlflow
+except ImportError:
+    mlflow = None
+
 
 def setup_logger(name="train", rank=0, level=logging.INFO):
     logger = logging.getLogger(name)
@@ -39,3 +44,24 @@ class Timer:
 
     def __exit__(self, *exc):
         self.elapsed = time.time() - self.t0
+
+
+def mlflow_init(experiment_name, run_name=None):
+    """Only the main rank should ever call this."""
+    if mlflow is None or not is_main_process():
+        return None
+    mlflow.set_experiment(experiment_name)
+    return mlflow.start_run(run_name=run_name)
+
+
+def mlflow_log_metrics(d, step=None):
+    if mlflow is None or not is_main_process():
+        return
+    for k, v in d.items():
+        mlflow.log_metric(k, v, step=step)
+
+
+def mlflow_log_params(d):
+    if mlflow is None or not is_main_process():
+        return
+    mlflow.log_params(d)
