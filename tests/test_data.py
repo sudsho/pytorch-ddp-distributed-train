6986a523364ab38856b775f7d7d0f0c6f4606436
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-11-25 21:46:04 +0530

    fix flaky test_data: seed rng before random crop

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,15 +5,16 @@ from src.data import get_transforms
 
 
 def test_train_transform_shape():
+    # seed the rng so RandomResizedCrop doesn't flake.
+    torch.manual_seed(0)
     tf = get_transforms(image_size=224, train=True)
-    # mock a PIL-ish image via tensor->PIL roundtrip is overkill, just call
-    # the underlying ops on a dummy uint8 tensor through ToPILImage at use time.
-    # Here we just check the pipeline exists and is callable.
     from PIL import Image
     img = Image.new("RGB", (320, 320), color=(123, 117, 104))
     out = tf(img)
     assert isinstance(out, torch.Tensor)
     assert out.shape == (3, 224, 224)
+    assert out.dtype == torch.float32
+    assert 0.0 <= out.min().item() and out.max().item() <= 1.0
 
 
 def test_eval_transform_shape():
